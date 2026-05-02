f2697e2967896476b4b6db221c4070a57ec4b14a
Salman Ahmed <sscvocational4@gmail.com>
2026-05-02 11:11:32 +0600

    Update page.tsx

--- frontend/app/movie/[id]/page.tsx
+++ frontend/app/movie/[id]/page.tsx
@@ -3,10 +3,15 @@ import { useParams } from 'next/navigation'
 import React from 'react'
 
 const MovieCard = () => {
-    const params = useParams()
-    const movieUrl = `https://vidsrc.xyz/embed/movie/${params.id}`
+  const params = useParams()
+  const movieUrl = `https://vidsrc.cc/embed/movie/${params.id}`
   return (
-    <iframe src={movieUrl} allowFullScreen allow='autoplay; encrypted-media; gyroscope; picture-in-picture' className='w-full h-screen' ></iframe>
+    <iframe
+      src={movieUrl}
+      allowFullScreen
+      allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+      className="w-full h-screen"
+    ></iframe>
   )
 }
 
